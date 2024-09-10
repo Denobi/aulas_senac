da8ee3bd4fee1781f5894816e16f624c36227559
Guilherme Denobi <guilherme.denobi46@gmail.com>
2024-09-10 20:28:02 -0300

    Create vetor.java

---
+++ JAVA/vetor.java
@@ -0,0 +1,46 @@
+package JAVA;
+
+import java.util.Scanner;
+/**
+ * vetor
+ */
+public class vetor {
+
+    public static void main(String[] args) {
+
+        Scanner vScanner = new Scanner(System.in);
+
+        int L = 0;
+        int C = 0;
+
+        int faixa = 0;
+        String linhao = "";
+
+        System.out.println("Digite a Linha >>>>>");
+        L = vScanner.nextInt();
+        System.out.println("Digite a Coluna >>>>>");
+        C = vScanner.nextInt();
+        System.out.println("Digite a faixa de valores >>>>>");
+        faixa = vScanner.nextInt();
+
+        int matriz [][] = new int[L][C];
+
+        for(int i = 0; i < matriz.length;i++){
+            for(int j = 0; j<matriz[0].length; j++){
+              matriz[i][j] = (int) (Math.random()*faixa);
+
+              if(j==0){
+                
+                  linhao = linhao + "" + matriz[i][j];
+                }else{
+                  linhao = linhao + "\t" + matriz[i][j];
+
+              }
+
+            }
+            System.out.println(linhao);
+            linhao="";
+        }
+        
+    }
+}
