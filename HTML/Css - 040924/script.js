1b65b070193cd919fa886827353ad8dbebcc8f36
Guilherme Denobi <guilherme.denobi46@gmail.com>
2024-09-11 22:03:37 -0300

    Aula 11/09/24

---
+++ HTML/Css - 040924/script.js
@@ -0,0 +1,9 @@
+const formArea = document.getElementById("frmContato");
+var nomeTxt = document.getElementById("txtNome");
+
+formArea.addEventListener("submit", function (event) {
+    if (nomeTxt.value == "teste") {
+        alert()
+    }
+});
+
