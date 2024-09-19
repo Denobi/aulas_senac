95b7846057fe0a52f2210f8d79747fa86009d830
Guilherme Denobi <34285566+Denobi@users.noreply.github.com>
2024-09-19 09:00:20 -0300

    18/09/2024

---
+++ HTML/Css - 040924/slide.js
@@ -0,0 +1,7 @@
+let atual = 0;
+const totalImagens = document.querySelectorAll(".carrossel-imagem");
+
+const carrosselContainer = document.querySelector(".carrosel-container");
+
+const anteriorBtn = document.querySelector(".carrossel-anterior");
+anteriorBtn.addEventListener('click',)
